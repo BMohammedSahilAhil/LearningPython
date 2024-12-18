087165ea21c1ef65c8364e4178432c4bc58e3fb0
B Mohammed Sahil <bmohammedsahil@C02CP2A8MD6M.local>
2024-12-18 20:13:37 +0530

    My First commit

---
+++ Day1/Printstatements.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Hello World","2+2",2+2,sep=" : ",end="\n")
+print("Hello World","2+2",2+2,sep=":",end="\t")
+print("Hello Boy","2+2",2+2,sep=":")
---
+++ Day1/Simplecalculator.py
@@ -0,0 +1,8 @@
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the Second number: "))
+
+print("The Sum of the two numbers is: ",a+b)
+print("The subtraction of the two numbers is: ",a-b)
+print("The multiplication of the two numbers is: ",a*b)
+print("The division of the two numbers is: ",a/b)
