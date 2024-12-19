728c7c46ce0b85fa8553379a36e2d4dbaf90fc30
B Mohammed Sahil <bmohammedsahil@C02CP2A8MD6M.local>
2024-12-19 08:40:31 +0530

    My 2nd day learniing of python

---
+++ Day2/VariablesWorkarounds.py
@@ -0,0 +1,23 @@
+#Variables
+from xmlrpc.client import boolean
+
+x=5
+pi=3.141
+name="John"
+a,b,c=5,3.2,"Hello"
+print(a)
+print(b)
+print(c)
+
+#DataTypes
+#1.Numbers
+#2.Strings
+#3.Boolean
+
+print(type(x))
+print(type(pi))
+print(type(name))
+
+#boolean
+is_sahil_married=boolean(input("Is Sahil married?"))
+print("is_sahil_married",is_sahil_married)
